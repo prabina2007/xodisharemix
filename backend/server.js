b3d00202a9e6402101f8e321c9f72900f67bd2c5
Prabina Kumar Sahu <prabinakumarsahu28@gmail.com>
2026-03-09 00:39:03 +0530

    xodisharemix initial commit

--- backend/server.js
+++ backend/server.js
@@ -36,6 +36,7 @@ app.get('*', (_req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
